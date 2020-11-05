4bc44dc8bb0951bd980de1890c764a44c31125e4
AndrewCopeland <andcope1995@gmail.com>
2020-11-05 11:04:18 -0500

    Add description to the connection

--- teamcity/teamcity-server/src/main/java/com/cyberark/ConjurProjectConnectionProvider.java
+++ teamcity/teamcity-server/src/main/java/com/cyberark/ConjurProjectConnectionProvider.java
@@ -1,6 +1,9 @@
 package com.cyberark;
 
 import jetbrains.buildServer.serverSide.oauth.OAuthProvider;
+import jetbrains.buildServer.serverSide.oauth.OAuthConnectionDescriptor;
+import java.util.Map;
+
 import jetbrains.buildServer.web.openapi.PluginDescriptor;
 
 
@@ -22,5 +25,18 @@ public class ConjurProjectConnectionProvider extends OAuthProvider {
 	@Override
 	public String getEditParametersUrl() {
 		return this.descriptor.getPluginResourcesPath("conjurconnection.jsp");
+	}
+
+	@Override
+	public String describeConnection(OAuthConnectionDescriptor connection) {
+    	ConjurJspKey keys = new ConjurJspKey();
+    	Map<String, String> params = connection.getParameters();
+
+    	String applianceUrl = params.get(keys.getApplianceUrl());
+    	String authnLogin = params.get(keys.getAuthnLogin());
+
+		String message = String.format("Connection to Cyberark Conjur server at '%s' with login '%s'",
+				applianceUrl, authnLogin );
+		return message;
 	}
 }
